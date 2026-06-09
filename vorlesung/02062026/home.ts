34e8ecc94817042c458688a12e6550eb64404c0a
slarasophie <Lara.Suter@Student.HTW-Berlin.de>
2026-06-09 21:49:46 +0200

    uebung6

---
+++ vorlesung/02062026/home.ts
@@ -0,0 +1,13 @@
+import{ Component } from '@angular/core';
+import { Right } from'.right/right';
+
+@Component({
+    selector: 'app-home',
+    imports: [Left, Right],
+    templateUrl: '.home.html',
+    styleUrl:'.home.css',
+})
+
+export class Home{
+    
+}
