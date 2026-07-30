4a2a26f521434eb34ccff54f209c5c33cfb924f2
slarasophie <Lara.Suter@Student.HTW-Berlin.de>
2026-07-30 22:34:17 +0200

    nav

--- WebtechProjekt2026/EXERTRACK/frontend/src/app/app.ts
+++ WebtechProjekt2026/EXERTRACK/frontend/src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Login } from './login/login';
+import { Registration } from './registration/registration';
+import { Create } from './create/create';
+import { Detail } from './detail/detail';
+import { Home } from './home/home';
+import { Nav } from './nav/nav';
+import { Table } from './table/table';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Login, Registration, Create, Detail, Home, Nav, Table],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
