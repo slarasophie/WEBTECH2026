9703cc8887c06acf58dc031eb1b661596eabc931
slarasophie <Lara.Suter@Student.HTW-Berlin.de>
2026-08-14 14:23:03 +0200

    frontend/backend

---
+++ WEBTECH_PROJEKT_2026/EXERTRACK/frontend/src/app/impressum/impressum.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impressum',
+  imports: [],
+  templateUrl: './impressum.html',
+  styleUrl: './impressum.css',
+})
+export class Impressum {}
